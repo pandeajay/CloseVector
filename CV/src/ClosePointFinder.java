c7f76a112fd3900e548a961ce3d5813579be568a
pandeajay <pandeaj@gmail.com>
2016-06-12 12:28:08 +0530

    26

--- CV/src/ClosePointFinder.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public interface ClosePointFinder {
-
-	//find close points and store
-	void findClosePoints(String file, int levels) throws Exception;
-
-}
--- CV/src/ResultWriter.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-import java.io.IOException;
-
-
-//interface for result writing
-public interface ResultWriter {
-	void flushResults(String fileName, String result) throws IOException;
-}
